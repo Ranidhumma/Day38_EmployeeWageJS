cc78dc4e3de243738a3538bfb02e0d38743355b9
Ranidhumma <ranidhumma8@gmail.com>
2022-04-04 21:25:37 +0530

    calculatng wage till condition reached

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,3 +1,7 @@
+/*
+Calculating Wages till Number of Working Days or Total Working
+Hours per month is Reached UC5
+*/
 const IS_ABSENT = 0 ;
 const IS_PART_TIME = 1 ;
 const IS_FULL_TIME = 2 ;
@@ -5,7 +9,8 @@ const PART_TIME_HOURS = 4 ;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20 ; 
 const NUM_OF_WORKING_DAYS = 20;
-//Uc4//
+const MAX_HRS_IN_MONTH = 160 ;
+
 function getWorkHrs(empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
@@ -22,12 +27,15 @@ function getWorkHrs(empCheck) {
 }
 
 let totalempHrs = 0;
-for (let day = 0 ; day <NUM_OF_WORKING_DAYS ; day++){
+let totalWorkingDays = 0;
+while (totalempHrs <= MAX_HRS_IN_MONTH &&
+        totalWorkingDays < NUM_OF_WORKING_DAYS){
+            totalWorkingDays++;
 let empCheck = Math.floor(Math.random() * 10) % 3;
 totalempHrs += getWorkHrs(empCheck);
 }
 let empWage = totalempHrs * WAGE_PER_HOUR;
-console.log("Hour: " +totalempHrs+ " Emp Wage: " + empWage)
+console.log("Total Days:"+totalWorkingDays + " Total Hour: " +totalempHrs+ " Emp Wage: " + empWage);
 
 
  
